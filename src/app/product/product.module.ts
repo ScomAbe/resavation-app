2e8ae6dcc1de9c43786ec45354dd449b13dd3131
ScomAbe <m.abe@scominc.co.jp>
2021-09-17 16:55:54 +0900

    Amgularフロントエンド側作成

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductListComponent } from './product-listings/product-listings.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+  {
+    path:'products',
+    component: ProductComponent,
+    children: [
+      {path:'', component: ProductListComponent},
+      {path:':productId', component: ProductDetailComponent}
+    ]
+  }
+];
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class ProductModule { }
